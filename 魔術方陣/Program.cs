f5c5ef752e3a93be4747ca2e605f86bbc6adc21c
edwinchen <edwinchen@jyic.net>
2022-07-03 07:31:11 +0800

    [新增功能] 魔術方陣原始程式碼C# / 魔術方陣原始程式碼C#
    [說明]
    1. 魔術方陣原始程式碼C#
    2.若有需要標註來源請通知我

---
+++ 魔術方陣/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace 魔術方陣
+{
+    class Program
+    {
+        public static int n = 5;
+        public static int[,] matrix= new int[n,n];
+        static void Main(string[] args)
+        {
+            Magic();
+            for(int i=0;i<n;i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j]+" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        public static void Magic()
+        {
+            int i, j, p, q, key;
+            for (i = 0; i < n; i++)
+                for (j = 0; j < n; j++)
+                    matrix[i,j] = 0;
+
+            matrix[0,(n - 1) / 2] = 1;
+
+            key = 2;
+            i = 0;
+            j = (n - 1) / 2;
+            while(key<=n*n)
+            {
+                p = (i - 1) % n;
+                q = (j - 1) % n;
+                if (p < 0) p = n - 1;
+                if (q < 0) q = n - 1;
+                if (matrix[p, q] != 0)
+                    i = (i + 1) % n;
+                else
+                {
+                    i = p;
+                    j = q;
+                }
+                matrix[i, j] = key;
+                key++;
+            }
+
+        }
+    }
+}
